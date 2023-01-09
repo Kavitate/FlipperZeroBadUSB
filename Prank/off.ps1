ff3d4388e98a9c85480eb7f4ef36917a1598bb9b
Kavitate <79380289+Kavitate@users.noreply.github.com>
2023-01-09 11:18:52 -0600

    Update off.ps1

--- Prank/off.ps1
+++ Prank/off.ps1
@@ -1,5 +1,4 @@
 function global:touchpad()
 {
     Disable-PnpDevice -InstanceId "HID\SYNA2393&COL02\5&10464366&0&0001" -Confirm:$false
-    Enable-PnpDevice -InstanceId "HID\SYNA2393&COL02\5&10464366&0&0001" -Confirm:$false
 }
